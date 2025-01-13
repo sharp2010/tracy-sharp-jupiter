12782ec0b7501af6179f87a00fbdbc1722fe3996
Tracy Sharp <tracysharp@Tracys-MacBook-Pro.local>
2025-01-13 16:21:30 -0500

    added copyrightvariable

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ document.body.appendChild(footer);
 //creating a variable named 'today' assigning it a new object
 const today = new Date();
 let thisYear = today.getFullYear();
+//adding variable named copyright
+let copyright = document.createElement('p');
 //adds the current year inside the <footer> element
-footer.innerHTML = `<p>&copy; ${thisYear} My Website.  All rights reserved.</p>`;
+footer.innerHTML = `<p>&copy; ${thisYear} Tracy Sharp's Website.  All rights reserved.</p>`;
 // adds the <footer> to the end of the body element
 document.body.appendChild(footer);
