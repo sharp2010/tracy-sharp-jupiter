3913751f455170c33bdfd41ab95f603fc57f9c8b
Tracy Sharp <tracysharp@Tracys-MacBook-Pro.local>
2025-01-13 16:08:40 -0500

    adding and changing code on index.js for footer

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@
 const footer = document.createElement('footer');
 // puts the <footer> to the end of the body element
 document.body.appendChild(footer);
-
+//creating a variable named 'today' assigning it a new object
 const today = new Date();
-let year = today.getFullYear();
-document.querySelector('footer');
+let thisYear = today.getFullYear();
+//adds the current year inside the <footer> element
+footer.innerHRML = `<p>&copy; ${thisYear} My Website.  All rights reserved.</p>`;
+// adds the <footer> to the end of the body element
+document.body.appendChild(footer);
